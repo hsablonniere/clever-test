196407d09b64e070c3438e5a2e60c551b410c5a9
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-01-09 15:24:02 +0100

    --wip--

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const server = http.createServer(function (req, res) {
   res.end('PONG')
 })
 
+setInterval(() => {
+	console.log('some logs', new Date())
+}, 500)
+
 
 server.listen(process.env.PORT)
